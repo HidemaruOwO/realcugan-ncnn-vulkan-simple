a8e718dedd56e6c8434f9faa136d9cba89a485bc
HidemaruOwO <hidemal@outlook.jp>
2022-09-19 19:07:44 +0900

    :bug: エラーハンドリング (setup.sh)

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@ function not_support {
 function create_bin {
   curl "$1" >> rcnvg.zip
   unzip rcnvg.zip
+  rm rcnvg.zip
   mv rcnvg bin
-
 }
 
 if type "curl" > /dev/null 2>&1; then
@@ -36,15 +36,15 @@ else
   not_support
 fi
 
-ARCH="$(uname -m)"
+CPU_ARCH="$(uname -m)"
 
-if [ "$(ARCH)" == "arm64" ] && ["$(OS)" == "Mac"]; then
+if [ "$CPU_ARCH" == "arm64" ] && [ "$OS" == "Mac" ]; then
   create_bin "https://01.raw.v-sli.me/realcugan-ncnn-vulkan/arm/mac/rcnvg.zip"
-elif [ "$(ARCH)" == "x86_64" ] && [ "$(OS)" == "Mac" ]; then
+elif [ "$CPU_ARCH" == "x86_64" ] && [ "$OS" == "Mac" ]; then
   create_bin "https://01.raw.v-sli.me/realcugan-ncnn-vulkan/x86_64/mac/rcnvg.zip"
-elif [ "$(ARCH)" == "x86_64" ] && [ "$(OS)" == "Cygwin" ]; then
+elif [ "$CPU_ARCH" == "x86_64" ] && [ "$OS" == "Cygwin" ]; then
   create_bin "https://01.raw.v-sli.me/realcugan-ncnn-vulkan/x86_64/windows/rcnvg.zip"
-elif [ "$(ARCH)" == "x86_64" ] && [ "$(OS)" == "Linux" ]; then
+elif [ "$CPU_ARCH" == "x86_64" ] && [ "$OS" == "Linux" ]; then
   create_bin "https://01.raw.v-sli.me/realcugan-ncnn-vulkan/x86_64/linux/rcnvg.zip"
 else
   not_support
